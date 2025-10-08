2f35bc3c6e8008e46eec2f3b00b9c26cd0719359
leoilankaras-byte <leoilankaras@gmail.com>
2025-10-08 13:36:33 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,7 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
-function nanoid(length = 6) {
-  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-  let result = '';
-  for (let i = 0; i < length; i++) {
-    result += chars.charAt(Math.floor(Math.random() * chars.length));
-  }
-  return result;
-}
-
-
+const { nanoid } = require("nanoid");
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
